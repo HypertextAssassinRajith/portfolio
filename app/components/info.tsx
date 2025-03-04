6cdf5a1d89446cc176d3c8ce11233a0e8f2fc3c9
Rajith Sanjaya <sanjayasenanayaka11@gmail.com>
2025-03-04 09:39:21 +0530

    feat: enhance Info component with blob SVG for profile image display

--- app/components/info.tsx
+++ app/components/info.tsx
@@ -4,6 +4,7 @@ import React, { useEffect, useState } from "react";
 import { useTheme } from "next-themes";
 import Profile from "@/app/assets/perfil.png"
 import Image from 'next/image'
+import Blob from '@/app/assets/blob.svg'
 
 export default function Info() {
   const { theme } = useTheme();
@@ -63,22 +64,29 @@ export default function Info() {
       <div className="flex flex-col md:flex-row items-center justify-center">
         <div className="md:w-1/2 flex justify-center relative mt-8 md:mt-0">
             <div className="w-96 h-96 md:w-80 md:h-80 relative flex items-center justify-center">
-            <svg viewBox="0 0 200 200" xmlns="http://www.w3.org/2000/svg">
-                <path
+            <svg viewBox="0 0 300 300" xmlns="http://www.w3.org/2000/svg">
+              <defs>
+                <clipPath id="blobClip">
+                  <path
+                    d="M62.2,-39.8C71.1,-20.4,62.3,5.2,49,23.4C35.7,41.6,17.8,52.2,-0.8,52.7C-19.4,53.2,-38.8,43.4,-51.3,25.7C-63.7,8.1,-69.1,-17.5,-59.4,-37.3C-49.7,-57.2,-24.8,-71.3,0.9,-71.8C26.6,-72.3,53.2,-59.2,62.2,-39.8Z"
+                    transform="translate(150 150) scale(1.2)"
+                  />
+                </clipPath>
+              </defs>
+              <image
+                href={Profile.src}
+                x="50"
+                y="20"
+                width="200"
+                height="200"
+                clipPath="url(#blobClip)"
+              />
+              <path
                 fill="#6E57E0"
                 d="M62.2,-39.8C71.1,-20.4,62.3,5.2,49,23.4C35.7,41.6,17.8,52.2,-0.8,52.7C-19.4,53.2,-38.8,43.4,-51.3,25.7C-63.7,8.1,-69.1,-17.5,-59.4,-37.3C-49.7,-57.2,-24.8,-71.3,0.9,-71.8C26.6,-72.3,53.2,-59.2,62.2,-39.8Z"
-                transform="translate(100 100)"
-                />
+                transform="translate(150 150) scale(1.2)"
+              />
             </svg>
-            <div className="absolute inset-0 flex items-center justify-center">
-                <Image
-                src={Profile}
-                alt="Rajith Sanjaya"
-                width={210}
-                height={200}
-                className="rounded-full"
-                />
-            </div>
             </div>
         </div>
     </div>
