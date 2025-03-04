2bf3e5cc6bdd404217a35b8370fdfb4dd1e0d5ba
Rajith Sanjaya <sanjayasenanayaka11@gmail.com>
2025-03-04 08:12:10 +0530

    feat: remove unused Image import from Info component

--- app/components/info.tsx
+++ app/components/info.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import React, { useEffect, useState } from "react";
-import Image from "next/image";
 import { useTheme } from "next-themes";
 import Profile from "@/app/assets/perfil.png"
 
