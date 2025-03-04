e4d7fea121e7026e6ea25b7c4596d3ae7065aa5f
Rajith Sanjaya <sanjayasenanayaka11@gmail.com>
2025-03-04 14:25:37 +0530

    fix: adjust layout and spacing in Info component for improved readability and consistency

--- app/components/info.tsx
+++ app/components/info.tsx
@@ -22,7 +22,7 @@ export default function Info() {
     >
       <div className="flex flex-col items-start lg:items-start lg:w-1/2 text-start lg:text-left">
         <h1 className="text-4xl font-bold">{"Hi, I'm Rajith Sanjaya"}</h1>
-        <h3 className="text-xl font-semibold text-gray-700 dark:text-gray-300">
+        <h3 className="text-xl font-semibold text-gray-700 dark:text-gray-300 mt-2">
           Full Stack Developer
         </h3>
         <p className="text-gray-600 dark:text-gray-400 mt-2">
@@ -38,19 +38,19 @@ export default function Info() {
       </div>
       <div className="flex content-center">
       <div className="grid h-56 grid-cols-1 content-evenly lg:hidden">
-          <a href="https://linkedin.com/in/sanjaya-senanayaka-976a32193" target="_blank" className="home__social-icon">
+          <a href="https://linkedin.com/in/sanjaya-senanayaka-976a32193" target="_blank" className="">
             <Linkedin />
           </a>
-          <a href="https://www.reddit.com/user/Rajith11" target="_blank" className="home__social-icon">
+          <a href="https://www.reddit.com/user/Rajith11" target="_blank" className="">
             <Slack />
           </a>
-          <a href="https://github.com/Hypertext-Assassin-RSS" target="_blank" className="home__social-icon">
+          <a href="https://github.com/Hypertext-Assassin-RSS" target="_blank" className="">
           <Github />
           </a>
         </div>
         <div className="flex flex-col lg:flex-row items-center justify-center w-full  lg:mt-0">
         <div className="lg:w-1/2 flex justify-center relative">
-          <div className="w-64 h-64 md:w-80 md:h-80 relative flex items-center justify-center">
+          <div className="w-96 md:w-80  sm:w-64 relative flex items-center justify-center">
             <svg viewBox="0 0 300 300" xmlns="http://www.w3.org/2000/svg" className="absolute">
               <defs>
                 <clipPath id="blobClip">
