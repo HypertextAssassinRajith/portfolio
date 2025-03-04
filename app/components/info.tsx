537337ac7ad4b7dc013746201c488b750e6338ab
Rajith Sanjaya <sanjayasenanayaka11@gmail.com>
2025-03-04 10:24:47 +0530

    feat: update Info component layout for improved responsiveness and adjust SVG scaling

--- app/components/info.tsx
+++ app/components/info.tsx
@@ -2,9 +2,8 @@
 
 import React, { useEffect, useState } from "react";
 import { useTheme } from "next-themes";
-import Profile from "@/app/assets/perfil.png"
-import Image from 'next/image'
-import Blob from '@/app/assets/blob.svg'
+import Profile from "@/app/assets/perfil.png";
+import Image from "next/image";
 
 export default function Info() {
   const { theme } = useTheme();
@@ -16,11 +15,11 @@ export default function Info() {
   if (!mounted) return null;
   return (
     <section
-      className={`flex flex-row md:flex-row items-center justify-center min-h-screen px-10 transition-colors duration-300 ${
+      className={`flex flex-col-reverse lg:flex-row items-center justify-evenly min-h-screen px-10 transition-colors duration-300 ${
         theme === "dark" ? "bg-gray-900 text-white" : "bg-white text-gray-900"
       }`}
     >
-      <div className="flex flex-col items-start md:w-1/2">
+      <div className="flex flex-col items-start lg:w-1/2">
         <div className="flex flex-col space-y-4 mb-6">
           <a
             href="https://linkedin.com/in/sanjaya-senanayaka-976a32193"
@@ -61,35 +60,34 @@ export default function Info() {
           Contact Me <i className="uil uil-envelope-upload"></i>
         </a>
       </div>
-      <div className="flex flex-col md:flex-row items-center justify-center">
-        <div className="md:w-1/2 flex justify-center relative mt-8 md:mt-0">
-            <div className="w-96 h-96 md:w-80 md:h-80 relative flex items-center justify-center">
-            <svg viewBox="0 0 300 300" xmlns="http://www.w3.org/2000/svg">
+      <div className="flex flex-col lg:flex-row items-center justify-center w-fit">
+        <div className="lg:w-fit flex justify-center relative mt-8 lg:mt-0">
+          <div className="w-96 h-96 lg:w-80 lg:h-80 relative flex items-center justify-center">
+            <svg viewBox="0 0 300 300" xmlns="http://www.w3.org/2000/svg" className="absolute">
               <defs>
                 <clipPath id="blobClip">
                   <path
                     d="M62.2,-39.8C71.1,-20.4,62.3,5.2,49,23.4C35.7,41.6,17.8,52.2,-0.8,52.7C-19.4,53.2,-38.8,43.4,-51.3,25.7C-63.7,8.1,-69.1,-17.5,-59.4,-37.3C-49.7,-57.2,-24.8,-71.3,0.9,-71.8C26.6,-72.3,53.2,-59.2,62.2,-39.8Z"
-                    transform="translate(150 150) scale(1.2)"
+                    transform="translate(100 100) scale(1.5)"
                   />
                 </clipPath>
               </defs>
-              <image
-                href={Profile.src}
-                x="50"
-                y="20"
-                width="200"
-                height="200"
-                clipPath="url(#blobClip)"
-              />
               <path
                 fill="#6E57E0"
                 d="M62.2,-39.8C71.1,-20.4,62.3,5.2,49,23.4C35.7,41.6,17.8,52.2,-0.8,52.7C-19.4,53.2,-38.8,43.4,-51.3,25.7C-63.7,8.1,-69.1,-17.5,-59.4,-37.3C-49.7,-57.2,-24.8,-71.3,0.9,-71.8C26.6,-72.3,53.2,-59.2,62.2,-39.8Z"
-                transform="translate(150 150) scale(1.2)"
+                transform="translate(150 150) scale(1.5)"
               />
             </svg>
-            </div>
+            {/* <Image
+              src={Profile.src}
+              alt="Profile Picture"
+              width={200}
+              height={200}
+              className="absolute z-10 rounded-full"
+            /> */}
+          </div>
         </div>
-    </div>
+      </div>
     </section>
   );
 }
