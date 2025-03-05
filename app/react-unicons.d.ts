249b7c6aa78bb8e32a0a09f0b1a338f677dd03c0
Rajith Sanjaya <sanjayasenanayaka11@gmail.com>
2025-03-05 14:56:53 +0530

    feat: add type definitions for react-unicons and update tsconfig for type roots

---
+++ app/react-unicons.d.ts
@@ -0,0 +1,10 @@
+declare module '@iconscout/react-unicons' {
+    import { FC } from 'react';
+  
+    export interface IconProps {
+      size?: number;
+      color?: string;
+    }
+  
+    export const UilWindowGrid: FC<IconProps>;
+  }
