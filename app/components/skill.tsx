97fb0f7860cbc3a4b4b639a29c0ddd8f626ee951
Rajith Sanjaya <sanjayasenanayaka11@gmail.com>
2025-03-05 14:43:15 +0530

    feat: enhance Skill component with icons and improved layout for better user experience

--- app/components/skill.tsx
+++ app/components/skill.tsx
@@ -2,6 +2,8 @@
 
 import React, { useEffect, useState } from "react";
 import { useTheme } from "next-themes";
+import { UilWindowGrid } from '@iconscout/react-unicons'
+import { ChevronDown } from "lucide-react";
 
 
 export default function Skill(){
@@ -19,8 +21,32 @@ export default function Skill(){
           }`}>
             <h1 className="text-4xl font-bold">Skills</h1>
             <span className="text-xl font-semibold text-gray-700 dark:text-gray-500 mt-2">My Skills</span>
-            <div className="flex flex-col">
-              
+            <div className="flex flex-col m-5">
+              <div className="flex justify-evenly items-center p-5">
+                <UilWindowGrid size = {50} color="#8200DB" />
+                <div>
+                  <h1 className="text-lg">Frontend Developer</h1>
+                  <span className="text-sm">More than 2 Years</span>
+                </div>
+                <ChevronDown />
+              </div>
+              <div className="flex justify-evenly items-center p-5">
+                <UilWindowGrid size = {50} color="#8200DB" />
+                <div>
+                  <h1 className="text-lg">Frontend Developer</h1>
+                  <span className="text-sm">More than 2 Years</span>
+                </div>
+                <ChevronDown />
+              </div>
+              <div className="flex justify-evenly items-center p-5">
+                <UilWindowGrid size = {50} color="#8200DB" />
+                <div>
+                  <h1 className="text-lg">Frontend Developer</h1>
+                  <span className="text-sm">More than 2 Years</span>
+                </div>
+                <ChevronDown />
+              </div>
+            
             </div>
         </section>
     );
