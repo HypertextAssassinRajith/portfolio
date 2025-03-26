949087138aa4fb34308705d3aae581c60bb17cc7
Rajith Sanjaya <sanjayasenanayaka11@gmail.com>
2025-03-26 08:36:09 +0530

    feat: enhance Services component layout and link interaction for improved user experience

--- app/components/service.tsx
+++ app/components/service.tsx
@@ -16,12 +16,12 @@ export default function Services() {
           {services.map((service, index) => (
             <div
               key={index}
-              className="bg-white shadow-md rounded-md p-6 flex flex-col items-center hover:shadow-lg transition"
+              className="bg-white shadow-md rounded-md p-6 flex flex-col items-center justify-center hover:shadow-lg transition h-64 w-64"
             >
               <div className="text-purple-600 text-3xl">{service.icon}</div>
               <h3 className="text-lg font-semibold mt-4 text-black">{service.title}</h3>
-              <a href={service.link} className="text-purple-600 mt-2 flex items-center">
-                View More <span className="ml-1">»</span>
+              <a href={service.link} className="text-purple-600 mt-2 flex items-center hover:text-purple-800 transition-transform decoration-300 group">
+                View More <span className="ml-1 transform transition-transform duration-300 group-hover:translate-x-1">»</span>
               </a>
             </div>
           ))}
