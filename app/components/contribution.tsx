21ed39d63e7ebf1bc45293480fe9d8b72c0f0977
Rajith Sanjaya <sanjayasenanayaka11@gmail.com>
2025-03-28 16:39:29 +0530

    feat: update Recent Contributions layout and styling; adjust image dimensions and improve header z-index

--- app/components/contribution.tsx
+++ app/components/contribution.tsx
@@ -39,12 +39,12 @@ export default function RecentContributions() {
   if (!mounted) return null;
 
   return (
-    <div className={`flex flex-col pt-23  lg:flex-row items-center justify-evenly  px-6 md:px-10 transition-colors duration-300  ${
+    <div className={`z-10 flex flex-col pt-23  items-center justify-evenly  px-6 md:px-10 transition-colors duration-300  ${
       theme === "dark" ? "bg-gray-900 text-white" : "bg-white text-gray-900"
     }`}>
-      <h2 className="text-4xl font-bold pt-10">Recently Contribute</h2>
-      <p className="text-xl font-semibold text-gray-700 dark:text-gray-500 mt-2 mb-20">Recent jobs</p>
-      <div className="relative w-full mx-auto ">
+      <h2 className="text-4xl font-bold pt-10">Contributions</h2>
+      <p className="text-xl font-semibold text-gray-700 dark:text-gray-500 mt-2 mb-15">Recent jobs</p>
+      <div className="relative w-full md:w-2/3 mx-auto ">
         <Swiper
           cssMode={true}
           pagination={true}
@@ -62,12 +62,12 @@ export default function RecentContributions() {
             <SwiperSlide key={index}>
               <div className="flex flex-col items-center justify-center w-2/3 gap-6 md:flex-row">
                 <div className="bg-gray-200 p-6 rounded-full shadow-md">
-                  <Image src={item.image} alt={item.title} className="w-24 h-24" width={500} height={500} />
+                  <Image src={item.image} alt={item.title} className="w-24 h-24" width={300} height={300} />
                 </div>
                 <div className="text-left">
                   <h3 className="text-lg font-semibold">{item.title}</h3>
                   <p className="text-gray-500">{item.description}</p>
-                  <a href={item.link} target="_blank" rel="noopener noreferrer" className="inline-flex items-center md:w-3/4 mt-6 px-6 py-3 w-2/4  bg-purple-600 text-white font-semibold rounded-lg shadow-md hover:bg-purple-700 space-x-2">
+                  <a href={item.link} target="_blank" rel="noopener noreferrer" className="inline-flex items-center  mt-6 px-6 py-3 w-fit  bg-purple-600 text-white font-semibold rounded-lg shadow-md hover:bg-purple-700 space-x-2">
                       Github <ChevronRight size={25} />  
                   </a>
                 </div>
